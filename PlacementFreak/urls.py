e6221bfe8c2e0bf3cd2a9650d4f03fe56df5304f
Varad Prabhu <varadp763@gmail.com>
2023-07-27 15:23:07 +0530

    Configured favicon for admin

--- PlacementFreak/urls.py
+++ PlacementFreak/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', lambda _ : redirect(settings.FAVICON_PATH, permanent=True)),
     path('',include('App.urls'))
 ]
 
+print(settings.FAVICON_PATH)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
